9cad9f72e28908192344a7f57eebe495da1ef8fe
lucien <yulu@guanghe.tv>
2017-04-24 16:53:00 +0800

    2017年04月24日16:52:42 mockData

---
+++ poem.js
@@ -0,0 +1,50 @@
+{
+  "poem": [
+    {
+      "url": "https://mvvideo5.meitudata.com/571090934cea5517.mp4",
+      "name": "明月几时有？把酒问青天。",
+      "desc": "明月从什么时候才开始出现的？我端起酒杯遥问苍天。",
+      "thumbnail": "https://raw.githubusercontent.com/smalldew/JSPatchDemoJS/master/flower.jpg"
+    },
+    {
+      "thumbnail": "https://raw.githubusercontent.com/smalldew/JSPatchDemoJS/master/flower.jpg",
+      "url": "https://mvvideo5.meitudata.com/571090934cea5517.mp4",
+      "desc": "不知道在天上的宫殿，今天晚上是何年何月。",
+      "name": "不知天上宫阙，今夕是何年。"
+    },
+    {
+      "url":"https://mvvideo5.meitudata.com/571090934cea5517.mp4",
+      "thumbnail": "https://raw.githubusercontent.com/smalldew/JSPatchDemoJS/master/flower.jpg",
+      "desc": "我想要乘御清风回到天上，又恐怕在美玉砌成的楼宇，受不住高耸九天的寒冷。",
+      "name": "我欲乘风归去，又恐琼楼玉宇，高处不胜寒。"
+    },
+    {
+      "url": "https://mvvideo5.meitudata.com/571090934cea5517.mp4",
+      "thumbnail": "https://raw.githubusercontent.com/smalldew/JSPatchDemoJS/master/flower.jpg",
+      "desc": "翩翩起舞玩赏着月下清影，哪像是在人间。",
+      "name": "起舞弄清影，何似在人间。"
+    },
+    {
+      "url": "https://mvvideo5.meitudata.com/571090934cea5517.mp4",
+      "thumbnail": "https://raw.githubusercontent.com/smalldew/JSPatchDemoJS/master/flower.jpg",
+      "desc": "月儿转过朱红色的楼阁，低低地挂在雕花的窗户上，照着没有睡意的自己。",
+      "name": "转朱阁，低绮户，照无眠。"
+    },
+    {      "url":"https://mvvideo5.meitudata.com/571090934cea5517.mp4",
+
+      "thumbnail": "https://raw.githubusercontent.com/smalldew/JSPatchDemoJS/master/flower.jpg",
+      "desc": "明月不该对人们有什么怨恨吧，为什么偏在人们离别时才圆呢？",
+      "name": "不应有恨，何事长向别时圆？"
+    },    {      "url":"https://mvvideo5.meitudata.com/571090934cea5517.mp4",
+
+      "thumbnail": "https://raw.githubusercontent.com/smalldew/JSPatchDemoJS/master/flower.jpg",
+      "desc": "人有悲欢离合的变迁，月有阴晴圆缺的转换，这种事自古来难以周全。",
+      "name": "人有悲欢离合，月有阴晴圆缺，此事古难全。"
+    },    {      "url":"https://mvvideo5.meitudata.com/571090934cea5517.mp4",
+
+      "thumbnail": "https://raw.githubusercontent.com/smalldew/JSPatchDemoJS/master/flower.jpg",
+      "desc": "只希望这世上所有人的亲人能平安健康，即便相隔千里，也能共享这美好的月光。",
+      "name": "但愿人长久，千里共婵娟。"
+    }
+  ]
+}
